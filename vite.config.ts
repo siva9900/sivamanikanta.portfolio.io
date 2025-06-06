4e764ed3370e8ae0bcf0fcc9d0e09746d2859b3e
siva9900 <sivamanichavvakula@gmail.com>
2025-06-07 03:41:55 +0530

    npm i

--- vite.config.ts
+++ vite.config.ts
@@ -3,25 +3,21 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-export default defineConfig({
-  plugins: [react()],
-})
-
-// // https://vitejs.dev/config/
-// export default defineConfig(({ mode }) => ({
-//   base: mode === 'production' ? '/' : '/',
-//   server: {
-//     host: "::",
-//     port: 8080,
-//   },
-//   plugins: [
-//     react(),
-//     mode === 'development' &&
-//     componentTagger(),
-//   ].filter(Boolean),
-//   resolve: {
-//     alias: {
-//       "@": path.resolve(__dirname, "./src"),
-//     },
-//   },
-// }));
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/sivamanikanta.portfolio.io/' : '/',
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
