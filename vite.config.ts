0321ea4e26b395305966bcad060a1d1fbf85c3ee
siva9900 <sivamanichavvakula@gmail.com>
2025-06-07 04:17:01 +0530

    config update

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,24 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react()],
   base: mode === "production" ? "/sivamanikanta.portfolio.io/" : "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+      },
+    },
+  },
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 }));
